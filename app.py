001f443b5838ecd7e337ec9276d5eed16974f8de
sunnyyadav54252-alt <sunnyyadav54252@gmail.com>
2026-02-11 13:48:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello! PaaS implementation using Google App Engine via GitHub"
+    return "PaaS implementation running successfully on Railway!"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
