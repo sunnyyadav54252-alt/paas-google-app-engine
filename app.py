77979550ede6242ba7684e1de267cf77a968b440
sunnyyadav54252-alt <sunnyyadav54252@gmail.com>
2026-02-11 14:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "PaaS implementation running successfully on Railway!"
+    return "Hello from Public PaaS"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
